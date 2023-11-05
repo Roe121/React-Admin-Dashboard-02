becd9339e7016aeea2a2d18d70d7c5062d543af4
Roe Lez <lezzarrami@gmail.com>
2023-11-05 12:58:03 +0100

    Compact/Expanded card transition on click

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,56 +1,77 @@
-import React, { useState } from 'react'
-import { AnimateSharedLayout } from 'framer-motion';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-import './Card.css'
+import React, { useState } from "react";
+import { AnimateSharedLayout } from "framer-motion";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./Card.css";
+import {UilTimes} from '@iconscout/react-unicons'
 
 const Card = (props) => {
-
-    const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <div className="Card">
-        <AnimateSharedLayout>
-            {
-                expanded? (
-                    'Expanded'
-                ): 
-                    <CompactCard param = {props}/>
-            }
-        </AnimateSharedLayout>
+      <AnimateSharedLayout>
+        {expanded ? (
+          <ExpendedCard param={props} setExpanded={() => setExpanded(false)} />
+        ) : (
+          <CompactCard param={props} setExpanded={() => setExpanded(true)} />
+        )}
+      </AnimateSharedLayout>
     </div>
-  )
+  );
+};
+
+// Compact Card
+
+function CompactCard({ param, setExpanded }) {
+  const Png = param.png;
+  return (
+    <div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      onClick={setExpanded}
+    >
+      <div className="radialBar">
+        <CircularProgressbar
+          value={param.barValue}
+          text={`${param.barValue}%`}
+        />
+        <span>{param.title}</span>
+      </div>
+      <div className="detail">
+        <Png />
+        <span>${param.value}</span>
+        <span>Last 24 hours</span>
+      </div>
+    </div>
+  );
 }
 
+// Expanded Card
 
+function ExpendedCard({ param, setExpanded }) {
+  return (
+    <div
+      className="ExpandedCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+    >
 
-// Compact Card 
-
-function CompactCard ({param}){
-    const Png = param.png;
-    return(
-        <div className="CompactCard"
-        style={{
-            background : param.color.backGround,
-            boxShadow : param.color.boxShadow
-        }}
-        >
-            <div className="radialBar">
-                <CircularProgressbar
-                value={param.barValue}
-                text={`${param.barValue}%`}
-                />
-                <span>{param.title}</span>
-            </div>
-            <div className="detail">
-                <Png/>
-                <span>${param.value}</span>
-                <span>Last 24 hours</span>
-            </div>
-        </div>
-    )
+      <div>
+        <UilTimes onClick={setExpanded}/>
+      </div>
+      <span> {param.title} </span>
+      <div className="chartContainer">
+       Chart
+      </div>
+      <span> Last 24h</span>
+    </div>
+  );
 }
 
-
-
-export default Card
+export default Card;
