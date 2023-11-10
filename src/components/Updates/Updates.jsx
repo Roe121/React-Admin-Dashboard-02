488aca785c6c8f9db1abb74a26631d15bed86ebd
Roe Lez <lezzarrami@gmail.com>
2023-11-10 01:52:03 +0100

    Finalized Updates component

--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { UpdatesData } from '../../Data/Data'
-import './Updates.css'
+import React from "react";
+import { UpdatesData } from "../../Data/Data";
+import "./Updates.css";
 
 const Updates = () => {
   return (
     <div className="Updates">
-        {UpdatesData.map((update)=>{
-            return(
-                <div className="update">
-                   <img src={update.img} alt="" />
-                   <div className="noti">
-                    <div>
-                      <span>{update.name}</span>
-                      <span>{update.noti}/</span>
-                    </div>
-                   </div>
-                </div>
-            )
-        })}
+      {UpdatesData.map((update) => {
+        return (
+          <div className="update">
+            <img src={update.img} alt="" />
+            <div className="noti">
+              <div style={{marginBottom: "0.5rem"}} >
+                <span>{update.name}</span>
+                <span> {update.noti}/</span>
+              </div>
+              <span>{update.time}</span>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Updates
+export default Updates;
